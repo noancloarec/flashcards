9c77d93136459a19305a4095a513084863e65af3
Noan Cloarec <noan.cloarec@gmail.com>
2024-04-30 19:01:00 +0200

    Order and filter cards

--- src/components/DeckList.vue
+++ src/components/DeckList.vue
@@ -1,34 +1,55 @@
 <script setup>
 
 // import decks from '../assets/data.json'
-import { ref } from 'vue';
-import {db} from '../utils/cards'
-import { collection, getDocs , where, query} from "firebase/firestore"; 
+import { computed, ref } from 'vue';
+import { db } from '../utils/cards'
+import { collection, getDocs, where, query, orderBy } from "firebase/firestore";
 
+/** @type {Ref.<Array.<Deck>>} */
 const decks = ref([])
 
 /**
  * @returns {Promise.<import('../utils/cards').Deck>}
  */
 const downloadDeckList = async () => {
-    const metadata = await getDocs(collection(db, 'deck_metadata'));
+    const deckMetadataRef = collection(db, 'deck_metadata')
+    const metadata = await getDocs(query(deckMetadataRef, orderBy("name")));
     return metadata.docs.map(d => ({
-        id : d.id,
+        id: d.id,
         ...d.data()
     }))
 }
 
+const searchTerm = ref("")
+
+const filteredDecks = computed(() => decks.value.filter(d => d.name.toLowerCase().includes(searchTerm.value.toLowerCase())))
 downloadDeckList().then(res => decks.value = res)
+
 
 </script>
 <template>
+    <input type="text" v-model="searchTerm" placeholder="Rechercher ...">
     <div>
-        <a v-for="deck in decks" :key="deck.id" :href="`/memorize/${deck.id}`">{{ deck.name }}</a>
+        <a v-for="deck in filteredDecks" :key="deck.id" :href="`/memorize/${deck.id}`">{{ deck.name }}</a>
     </div>
 </template>
 
 <style scoped>
+input{
+    width: 100%;
+    border-radius: 20px;
+    background-color: #673AB7;
+    border: none;
+    height: 55px;
+    text-align: center;
+    color: white;
+}
+input::placeholder{
+    color: white
+}
+
 div {
+    margin-top: 20px;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 20px;
@@ -51,7 +72,6 @@ a {
     text-align: center;
     color: #29162c;
     text-decoration: none;
-    padding: 5px;
     min-height: 100px;
 }
 </style>
